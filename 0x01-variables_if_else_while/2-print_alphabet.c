284f655a86483d5ec22b37cec5d072eb81d3e9ab
Bii-lang <enidbii0@gmail.com>
2022-06-04 22:08:50 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/** 
+ * main - Prints out the alphabet in small letters
+ * when it executes
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
