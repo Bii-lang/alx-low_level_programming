e8d5fdac2426d6f804abf4932b5ac63785daaa55
Bii-lang <enidbii0@gmail.com>
2022-06-05 01:09:57 -0700

    alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,6 +6,21 @@
  */
 int main(void)
 {
+	char c ='a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n);
+	return (0);
+}
+int main(void)
+{
 	char c = 'a';
 
 	while (c <= 'z')
