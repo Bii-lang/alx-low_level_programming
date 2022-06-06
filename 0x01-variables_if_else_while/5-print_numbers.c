f10014c8ef36725cc266656c3bbf03a403316d20
Bii-lang <enidbii0@gmail.com>
2022-06-06 10:15:36 -0700

    add

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,12 @@
  * main - prints out all single digits of base ten when executed
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int a = 0;
 
 	while (a < 10)
-       	{
+	{
 		printf("%d", a);
 		a++;
 	}
