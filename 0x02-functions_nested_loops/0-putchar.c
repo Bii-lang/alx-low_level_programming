362c5747ded2f5bfd8758c8ec1b95da967825745
Bii-lang <enidbii0@gmail.com>
2022-06-08 13:34:12 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-	char str[];
-	int i = 0;
-	while (str[i] == '_putchar')
-	{
-		_putchar(str[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
-}
