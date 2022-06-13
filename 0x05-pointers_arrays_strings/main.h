c6b985ec38f01af0c8f3f40cf7ef1b359c02efb6
Bii-lang <enidbii0@gmail.com>
2022-06-13 11:04:27 -0700

    update2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,21 +1,5 @@
 #include <unistd.h>
-/**
- *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
 /**
  * reset_to_98 - takes a pointer to an int and resets 
  * it to 98
