a194a82b18430e0fcc84a405f9a60aeb07aba326
Bii-lang <enidbii0@gmail.com>
2022-06-13 12:42:21 -0700

    resets to ninety eight

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer and updates the value it points
  * to to a value
- * @n - input integer.
+ * @n: input integer.
  * Return: Nothing
  */
 void reset_to_98(int *n)
