50134b5f4ce359bb47bd15e08ff4ec327eed9c89
Bii-lang <enidbii0@gmail.com>
2022-06-13 12:53:51 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers when executed
+ * @a: input integer
+ * @b: input integer
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
