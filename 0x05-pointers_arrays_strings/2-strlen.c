c8ffea991cce9a63b7ff61f9eee021a25029ea69
Bii-lang <enidbii0@gmail.com>
2022-06-13 13:13:45 -0700

    strlength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input character
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int count;
+	for (count = 0; count < '\0'; count++)
+	{
+		if (*(s + count) != '\0')
+	}
+	return(count);
+}
