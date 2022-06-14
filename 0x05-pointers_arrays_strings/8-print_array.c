6188f0ea17fb0668dfe2592b5d42c230ddbb632c
Bii-lang <enidbii0@gmail.com>
2022-06-14 09:28:44 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * print_array - prints elements of an array of integers
+ * @a: input integer
+ * @n: input integer
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int b = 0;
+
+	for (; b < n; b++)
+	{
+		printf("%d", *(a + b));
+		if (b != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
