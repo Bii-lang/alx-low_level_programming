d08cf63248704926de0e01eac9604d7d2540241c
Bii-lang <enidbii0@gmail.com>
2022-06-15 09:50:26 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_alphabet - prints the alphabet in 10 lines followed by a new line
  */
 void print_alphabet_x10(void)
 {
 	char c;
-	int i;
-	while (i < 11)
+	int a;
+
+	for (a = 0; a < 10; a++)
 	{
-		_putchar('\n');
-		i++;
 		for (c = 'a'; c <= 'z'; c++)
-		{
 			_putchar(c);
-		}
+		_putchar('\n');
 	}
 }
