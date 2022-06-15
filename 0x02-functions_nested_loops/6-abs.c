7848151fa451f51f41ae626e44c8d066ff22441a
Bii-lang <enidbii0@gmail.com>
2022-06-15 10:43:01 -0700

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: input integer
+ * Return: Absolute value of a number
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
+
