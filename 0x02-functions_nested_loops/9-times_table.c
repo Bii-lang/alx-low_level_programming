436f1965a50e5c090d98e532d2f216aafe765c04
Bii-lang <enidbii0@gmail.com>
2022-06-15 12:52:51 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,10 +10,10 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b =0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			k = a * b;
-			if ((k /10) == 0)
+			if ((k / 10) == 0)
 			{
 				if (b != 0)
 					_putchar(' ');
