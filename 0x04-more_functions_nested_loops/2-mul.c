4dc0c427bc52e34328d7f98176352dc1aeabfab9
Bii-lang <enidbii0@gmail.com>
2022-06-15 13:59:48 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: integer
+ * @b: integer
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int multiply;
+	
+	multiply = a * b;
+	return (multiply);
+}
