4ff407d485adcc543b1ff67845167085eafde913
Bii-lang <enidbii0@gmail.com>
2022-06-15 10:20:18 -0700

    which is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks holberton
+ * @c: input character
+ * Descrption: checks if character is lowercase letter or uppercase
+ * Return: 1 if its a letter, lowercase or uppercase or 0 otherwise
+ */
+int _isalpha(int c)
+{
+	char low, upp;
+	int isLet = 0;
+	
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		for (upp = 'A'; upp <= 'Z'; upp++)
+		{
+			if (c == low || c == upp)
+				isLet = 1;
+		}
+	}
+	return (isLet)
+}
